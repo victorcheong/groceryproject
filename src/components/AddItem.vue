c444c879bc856f7be6a749e9f404ccbeb13c54e7
Victor <vcjw97@gmail.com>
2020-03-03 10:00:13 +0800

    Test Project
    Testing

---
+++ src/components/AddItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id='app'>
+        <h1>Add Item Page</h1>
+        <form id="fm1">
+            <label>Item Name</label>
+            <input type="text"/>
+            <label>Item
+            Category</label>
+            <input type="text"/>
+        </form>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="headerStyle">
+    <header>
+        <h1>{{ msg }}</h1>
+        <ul>
+          <li><router-link to='/' exact>List</router-link></li>
+          <li><router-link to='/add' exact>Add Item</router-link></li>
+        </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data(){
+    return{
+        msg: 'Grocery List'
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header{
+    background:rgb(122, 63, 100);
+    padding:10px;
+    border-style: solid;
+    border-color:black;
+    border-width: 2px;
+}
+h1{
+    color:ivory
+}
+</style>
